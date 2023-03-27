6880a4679b905a4ce078e904d8b5f8bc30454888
OLESIA TUR <olesia.tur@gmail.com>
2023-03-27 17:28:17 +0200

    component 2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,28 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  var pokemon = pokemonList[1]; // on récupère le deuxième pokémon du tableau
+
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {/* on affiche l'image si elle est présente, sinon on affiche un paragraphe */}
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
 
 export default PokemonCard;
-
