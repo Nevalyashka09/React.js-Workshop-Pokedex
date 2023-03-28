8d962cbc1533fb095f840e6231c5955202a4577a
OLESIA TUR <olesia.tur@gmail.com>
2023-03-28 17:09:34 +0200

    props.1

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,20 +1,8 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  var pokemon = pokemonList[1]; // on récupère le deuxième pokémon du tableau
+function PokemonCard(props) {
+  let pokemon = props.pokemon;
 
   return (
     <figure>
-      {/* on affiche l'image si elle est présente, sinon on affiche un paragraphe */}
       {pokemon.imgSrc ? (
         <img src={pokemon.imgSrc} alt={pokemon.name} />
       ) : (
