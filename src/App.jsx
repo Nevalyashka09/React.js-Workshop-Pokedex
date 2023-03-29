6978397502c4bbd142acd878b3c5b74df4c1d438
OLESIA TUR <olesia.tur@gmail.com>
2023-03-29 12:18:07 +0200

    state

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
-let pokemonList = [
+const pokemonList = [
   {
     name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
     name: "mew",
@@ -16,9 +31,27 @@ let pokemonList = [
 ];
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handlePreviousClick = () => {
+    setPokemonIndex((prevIndex) => prevIndex - 1);
+  };
+
+  const handleNextClick = () => {
+    setPokemonIndex((prevIndex) => prevIndex + 1);
+  };
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <div>
+        {pokemonIndex > 0 && (
+          <button onClick={handlePreviousClick}>Précédent</button>
+        )}
+        {pokemonIndex < pokemonList.length - 1 && (
+          <button onClick={handleNextClick}>Suivant</button>
+        )}
+      </div>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
     </div>
   );
 }
